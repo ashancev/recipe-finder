0abca4d04d03fff7050bee168b24e9ae41d6b63b
Shancev Andrei <a.shancev@icloud.com>
2021-12-29 23:43:17 +0300

    add login page and routing

---
+++ src/app/app.routing.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import {MainPageComponent} from './pages/main-page/main-page.component';
+import { AuthGuard } from './guards/auth.guard';
+
+const routes: Routes = [
+  { path: '', component: LoginPageComponent},
+  { path: 'login', component: LoginPageComponent },
+  { path: 'index', component: MainPageComponent, canActivate: [AuthGuard] },
+];
+
+@NgModule({
+  imports: [CommonModule, BrowserModule, RouterModule.forRoot(routes)],
+  exports: []
+})
+export class AppRoutingModule {}
---
+++ src/app/pages/login-page/login-page.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import {AuthService} from '../../services/auth.service';
+import {FormControl, FormGroup} from '@angular/forms';
+import { Router } from '@angular/router';
+
+
+@Component({
+  selector: 'app-login-page',
+  templateUrl: './login-page.component.html',
+  styleUrls: ['./login-page.component.css']
+})
+export class LoginPageComponent implements OnInit {
+
+  public formGroup: FormGroup;
+
+  constructor(private authService: AuthService,  private router: Router) {
+    this.authService = authService;
+    this.formGroup = new FormGroup({
+      login: new FormControl(''),
+      password: new FormControl(''),
+    });
+  }
+
+  ngOnInit() {
+    this.checkIfAuthenticated();
+  }
+
+  onLoginClick() {
+    this.authService.signIn(this.formGroup.get('login').value, this.formGroup.get('password').value)
+      .subscribe(
+        () => this.router.navigate(['index']),
+        (error) => {
+          console.log(error);
+        }
+      );
+  }
+
+  private checkIfAuthenticated(): void {
+    if (this.authService.isSignedIn()) {
+      this.router.navigate(['index']);
+    }
+  }
+
+}
